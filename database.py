856ddede6fe1f1bf136c250f8bfb0ccc155a4fc3
Yash vishwanathan <yashvishwanathan@gmail.com>
2026-04-30 15:29:23 +0530

    Fix indentation and formatting in database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-db_URL = "postgresql://postgres:Harini18@localhost:5432/fastapi_demo"
+
 engine = create_engine(db_URL)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
